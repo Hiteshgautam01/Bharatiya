caf42286e663948964586b73e20f1e2f903bc093
Pankaj-Baranwal <pankajisadeveloper@gmail.com>
2018-03-21 07:11:23 +0530

    Backened of ProfileActivity

--- bjp/recordCommentsActivity.php
+++ bjp/recordCommentsActivity.php
@@ -6,10 +6,10 @@
 	$post_id = $_POST['post_id'];
 	$statement = $pdo->prepare("CREATE TABLE IF NOT EXISTS comments (id INT AUTO_INCREMENT, user_id INT NOT NULL, post_id INT NOT NULL, message VARCHAR(2038), PRIMARY KEY(id));");
 	$statement->execute();
-	$statement = $pdo->prepare("INSERT INTO action (user_id, post_id, message) VALUES (?, ?, ?)");
+	$statement = $pdo->prepare("INSERT INTO comments (user_id, post_id, message) VALUES (?, ?, ?)");
 	$statement->execute([$user_id, $post_id, $message]);
-	$stmt = $pdo->prepare("SELECT id FROM comments WHERE user_id = '$user_id' AND post_id = '$post_id' AND message = '$message' LIMIT 1;");
-    $stmt->execute();
+	$stmt = $pdo->prepare("SELECT id FROM comments WHERE user_id = ? AND post_id = ? AND message = ? LIMIT 1;");
+    $stmt->execute([$user_id, $post_id, $message]);
     $data = $stmt->fetch(PDO::FETCH_ASSOC);
     $stmt->closeCursor();
     echo $data['id'];
